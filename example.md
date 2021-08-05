718c19bde6c9e82431415bc81f87e4f7fd565ff5
Will Berner <will.o.berner@gmail.com>
2021-08-05 01:50:24 -0400

    Started on license generation. Created example readme to refernce.

---
+++ example.md
@@ -0,0 +1,22 @@
+# [Title](liveLink)
+
+## Table of Contents
+
+Description
+
+## Installation Instructions
+
+## Instructions for use
+
+## Testing Instructions
+
+## Contribution Guidelines
+
+## License
+
+## Questions
+
+    * Question one
+        * Answer one
+    * Question two
+        * Answer two
--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 // TODO: Include packages needed for this application
 const generateMarkdown = require("./utils/generateMarkdown.js")
 const inquirer = require("inquirer");
-const questions = require("./lib/questions.js")
+const questions = require("./lib/questions.js");
+const fs = require("fs");
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        (error) ? log.error(error) : console.log("Succes");
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
 
     inquirer.prompt(questions)
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+        console.log(generateMarkdown(data));
+    })
     .catch(error => console.log(error));
 
     // console.log(generateMarkdown({title:"test"}));
